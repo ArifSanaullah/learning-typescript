cbda305e2b788544c3d53141b5f49348af74c7fc
Arif Sanaullah <ak7896023@gmail.com>
2020-12-15 12:53:00 +0500

    feat: unknown && never

--- using-types/unknown-never.ts
+++ using-types/unknown-never.ts
@@ -1,14 +1,18 @@
-var userInput;
-var userName;
+let userInput: unknown;
+let userName: string;
+
 userInput = 4;
 userInput = "Max";
+
 // userName = userInput; // causes an error and that's good. With this, typescript forces us to do a typecheck before we assign unknown type of value to a known(string) type of value. like following
 if (typeof userInput === "string") {
-    userName = userInput; // now, this works fine.
+  userName = userInput; // now, this works fine.
 }
-var throwError = function (message, code) {
-    // it's return type is never
-    throw { message: message, code: code };
+
+const throwError = (message: string, code: number) => {
+  // it's return type is never
+  throw { message, code };
 };
+
 console.log(throwError("An error occured", 500)); // will show nothing in consol. not even undefined
 throwError("An error occured", 500);
